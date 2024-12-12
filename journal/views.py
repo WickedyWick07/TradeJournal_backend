db81d47f3404d190e86c56392358629c486d94fc
Shaun <nhlamulosmashaba@gmail.com>
2024-12-12 15:54:51 +0200

    Image Uploading

--- journal/views.py
+++ journal/views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import JournalEntry, AccountJournal
-from .serializers import JournalEntrySerializer, AccountJournalSerializer
+from .models import JournalEntry, AccountJournal, JournalImage
+from .serializers import JournalEntrySerializer, AccountJournalSerializer, JournalImageSerializer
 from django.conf import settings 
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
