40d3ec5c37aea50499309924b3e2a3d557bc9de7
Shaun <nhlamulosmashaba@gmail.com>
2024-12-15 14:01:50 +0200

    feat: database reversion

--- AiJournal/settings.py
+++ AiJournal/settings.py
@@ -205,16 +205,21 @@ WSGI_APPLICATION = 'AiJournal.wsgi.application'
 
 # Add these at the top of your settings.py
 
-load_dotenv()
+# Add these at the top of your settings.py
+from os import getenv
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+# Replace the DATABASES section of your settings.py with this
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DBNAME'),       # Database name
-        'USER': os.getenv('USER'),         # Database user
-        'PASSWORD': os.getenv('PASSWORD'), # Database password
-        'HOST': os.getenv('HOST'),         # Host (e.g., aws-0-us-east-2.pooler.supabase.com)
-        'PORT': os.getenv('PORT'),         # Port (e.g., 5432)
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
 }
 
